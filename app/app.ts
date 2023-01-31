2ca901ca039ed5eb0e98708bf3af4d581c7d34db
Nashdidan <nash.idan@gmail.com>
2023-01-31 11:26:33 +0200

    working express

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
-import express from "express"
+import express = require('express')
 const app = express()
 const port = 3000
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,5 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-export default app
